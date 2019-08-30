2779afe29bf12ebeab2d466d3a80167b1167bf08
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-08-30 15:54:28 -0400

    added a port listener and added book.js file

--- app.js
+++ app.js
@@ -6,5 +6,9 @@ app.use(morgan('dev'));
 const port = 8000;
 
 app.listen(port, () => {
-    console.log(`Express server is runnig at ${port}`)
+    console.log(`Express server is runnig at port: ${port}`)
+})
+
+app.get('/', (_, res) => {
+    res.send(`App is running at port: ${port}`)
 })

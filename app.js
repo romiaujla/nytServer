3615cb2fb5d12d48bff99d257642fe8fdca93916
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-08-30 15:59:19 -0400

    imported books and checking if import is successful

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 const express = require('express');
 const morgan = require('morgan');
+const books = require('./book');
 
 const app = express();
 app.use(morgan('common'));
 const port = 8000;
 
+// Listening at port = 8000
 app.listen(port, () => {
     console.log(`Express server is runnig at port: ${port}`)
 })
 
+// default path
 app.get('/', (_, res) => {
-    res.send(`App is running at port: ${port}`)
+    res
+      .send(`App is running at port: ${port}`)
 })
 
 app.get('/book', (req, res) => {
-    res.send(`Reached Books`);
+    
+    // return the books to see if import is working
+    res
+      .json(books);
 })
